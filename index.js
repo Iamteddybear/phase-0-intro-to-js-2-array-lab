0b68aa121093810946ddd5ded10e1b72d791f33e
ned <nedschultz@outlook.com>
2022-12-04 17:11:51 +1000

    commit tree 16f99352c279813d463cab7f1c8c243a87e4f6b3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(NewCatNames) {
+    cats.push(NewCatNames);
+}
+
+function destructivelyPrependCat(NewCatNames) {
+    cats.unshift(NewCatNames);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+   let copyOfCats = [...cats];
+   copyOfCats.push(name);
+   return copyOfCats;
+}
+
+function prependCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+
+function removeLastCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.pop(name);
+    return copyOfCats;
+}
+
+function removeFirstCat(name) {
+    let copyOfCats = [...cats];
+    copyOfCats.shift(name);
+    return copyOfCats;
+}
